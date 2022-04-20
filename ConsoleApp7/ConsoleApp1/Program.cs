72476d403be4bea1d12d4faeb3c092ca5ec8990d
hrafag <afagnd@code.edu.az>
2022-04-20 18:53:17 +0400

    New tasks are added

---
+++ ConsoleApp7/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "bU tasKI metHodla yAzmAliSiz";
+            string str1 = str.ToLower();
+
+            string[] substr1 = str1.Split(' ');
+            foreach (var sub in substr1)
+            {
+                Console.WriteLine($"substring: {sub}");
+            }
+
+            //string str2 = str1.ToTitleCase();
+            //Console.WriteLine(str2);
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Afag";
+            student.Surname = "Dadashova";
+            student.Group = "P324";
+            student.Graduated = "Is graduated";
+
+            Console.WriteLine(student.getInfo());
+            
+        }
+        
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public string Graduated;
+
+        public string getInfo()
+        {
+            return $"Name:{Name}, Surname:{Surname}";
+
+
+        }
+              
+              
+    }
+        
+            
+        
+    
+}
